b264a2824557d37df5e27b07c9c902e548dfb074
nuri <kimnuri@gimnuliui-iMac.local>
2022-08-15 22:09:03 +0900

    상세페이지 구현

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import Footer from "../components/Footer";
+import Router from "next/router";
+
+import ProgressBar from "@badrap/bar-of-progress";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const progress = new ProgressBar({
+    size: 4,
+    color: "#FE595E",
+    className: "z-50",
+    delay: 100,
+  });
+
+  // ProgressBar 를 next.js 에 연결
+  Router.events.on("routeChangeStart", progress.start);
+  Router.events.on("routeChangeComplete", progress.finish);
+  Router.events.on("routeChangeError", progress.finish);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
